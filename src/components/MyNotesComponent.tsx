b1e7c5314b8688b728b66549b3874729de42f1eb
Baren <baren1998@gmail.com>
2020-04-14 23:16:12 +0700

    Сделал мало-мальский роутинг

---
+++ src/components/MyNotesComponent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class MyNotesComponent extends React.Component
+{
+    constructor(props: any)
+    {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>
+                Мои заметки
+            </h1>
+        );
+    }
+}
